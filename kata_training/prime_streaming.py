685e3516855fda5e93b145bed4821dfaa3e60e9a
pdaambrosio <pda.ambrosio@gmail.com>
2022-11-30 11:56:03 -0300

    fix: revert commits

---
+++ kata_training/prime_streaming.py
@@ -0,0 +1,14 @@
+class Primes:
+    @staticmethod
+    def stream():
+        
+            
+            
+def main() -> None:
+    stream = Primes.stream()
+    for _ in range(10):
+        print(next(stream))
+    
+    
+if __name__ == '__main__':
+    main()
