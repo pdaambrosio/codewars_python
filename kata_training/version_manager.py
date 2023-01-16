5610a5a12caaee007a87cb15af4dc164dd1a448f
pdaambrosio <pda.ambrosio@gmail.com>
2023-01-16 15:30:36 -0300

    feat: assert

--- kata_training/version_manager.py
+++ kata_training/version_manager.py
@@ -1,7 +1,12 @@
 class VersionManager:
-    def __init__(self, version):
-        self.version = version
+    def __init__(self, version: str = '0.0.1'):
+        self.version = version[:5]
+        assert any(i.isalpha() for i in self.version.replace('.', '')) != True, 'Invalid version format'
 
 
-t = VersionManager("1.2.3.3")
+t = VersionManager('1.a.3.4')
 print(t.version)
+
+
+test = '1.a.3.4'
+print([i.isalpha() for i in test.replace('.', '')])
