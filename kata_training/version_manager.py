6374919bc97b3801084bd171b0fa5f2c3dc63e75
pdaambrosio <pda.ambrosio@gmail.com>
2023-01-20 07:31:52 -0300

    test: functions

--- kata_training/version_manager.py
+++ kata_training/version_manager.py
@@ -1,12 +1,34 @@
+from __future__ import annotations
+
 class VersionManager:
     def __init__(self, version: str = '0.0.1'):
         self.version = version[:5]
-        assert any(i.isalpha() for i in self.version.replace('.', '')) != True, 'Invalid version format'
+        if version == '':
+            self.version = '0.0.1'
+        assert any(i.isalpha() for i in self.version.replace('.', '')) != True, 'Error occurred while parsing version!'
+
+    def release(self: any) -> str:
+        return self.version
+
+    def major(self: any) -> VersionManager:
+        self.version = str(int(self.version[0]) + 1) + '.0.0'
+        return self
+
+    def minor(self: any) -> VersionManager:
+        self.version = self.version[0] + '.' + str(int(self.version[2]) + 1) + '.0'
+        return self
+
+    def patch(self: any) -> VersionManager:
+        self.version = self.version[0] + '.' + self.version[2] + '.' + str(int(self.version[4]) + 1)
+        return self
+
+    def rollback(self: any) -> VersionManager:
+        if self.version > 0:
+            self.version = self.version[:-1]
+        else:
+            raise Exception('Cannot rollback!')
+        return self
 
 
-t = VersionManager('1.a.3.4')
-print(t.version)
-
-
-test = '1.a.3.4'
-print([i.isalpha() for i in test.replace('.', '')])
+t = VersionManager('1.2.3')
+print(t.release())
