948edfe810e49fe35bc64ba834995a9d2120d332
pdaambrosio <pda.ambrosio@gmail.com>
2023-04-03 06:47:39 -0300

    doc: reverse words

--- kata_training/reverse_words.py
+++ kata_training/reverse_words.py
@@ -1,4 +1,13 @@
 def reverse_words(text: str) -> str:
+    """
+    "Join the reversed words in the text, separated by spaces."
+    
+    The function is a bit more complicated than that, but that's the gist of it
+    
+    :param text: str - the string to reverse
+    :type text: str
+    :return: A string of the words in the text in reverse order.
+    """
     return ' '.join(word[::-1] for word in text.split(' '))
 
 
